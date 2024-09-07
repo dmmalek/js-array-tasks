0d9172621f39daff8eb4917e2c4c386137da4c36
Abdul Malek <abdulmalekmonir97@gmail.com>
2024-09-07 23:11:30 +0600

    solve 4 and 5 no problem

--- array-problem.js
+++ array-problem.js
@@ -6,11 +6,11 @@
 // change the value of the 2nd index element to jambura
 // console log the final array
 
-// const fruits = ["apple", "banana", "malta", "pineapple"];
-// console.log(fruits[2]);
+const fruits = ["apple", "banana", "malta", "pineapple"];
+console.log(fruits[2]);
 
-// fruits[1] = "orange";
-// console.log(fruits);
+fruits[1] = "orange";
+console.log(fruits);
 
 // Declare an array of 3 tourist destinations
 // Add a new tourist destination to your tourist array
@@ -18,11 +18,11 @@
 // Remove the last tourist destination you have added
 // display the final array as output
 
-// const destination = ['Dhaka', 'Barishal', 'Chattaogram'];
-// destination.push('Khulna');
-// destination.push('Mymensing', 'kushtiya');
-// destination.pop();
-// console.log(destination);
+const destination = ['Dhaka', 'Barishal', 'Chattaogram'];
+destination.push('Khulna');
+destination.push('Mymensing', 'kushtiya');
+destination.pop();
+console.log(destination);
 
 // 3. Checking Array Membership with ‘includes’
 // Instructions:
@@ -34,3 +34,39 @@
 const books = ['Bangla', 'English', 'Math', 'Science'];
 console.log(books.includes('javascript book'));
 console.log(Array.isArray(books));
+
+// 4. Checking if it's an Array
+// Instructions:
+
+// Create different variables, each containing either an array or a non - array value.
+
+// Now use isArray to check if each variable is an array.
+
+// Print a message to the console indicating whether each variable is an array or not.
+
+let games = ['cricket', 'footbal', 'volleyball', 'badminton'];
+let num = 50;
+
+let checkArray = Array.isArray(num);
+if (checkArray) {
+    console.log('this is array');
+}
+else {
+    console.log('Not Array');
+}
+
+
+console.log(Array.isArray(games));
+console.log(Array.isArray(num));
+
+// 5. Combining Arrays
+// Instructions:
+
+// Create two arrays of your choice.
+// Use the concat method to combine the two arrays into a new array.
+// Print both the original arrays and the combined array using console.log().
+
+let animals = ['cat', 'dog', 'cow', 'goat', 'buffelo'];
+let legs = ['two', 'four', 'four', 'four', 'four'];
+
+console.log(animals.concat(legs));
