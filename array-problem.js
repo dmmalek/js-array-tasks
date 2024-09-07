be591456dc33eb011da1e01ec16a5126d9420e17
Abdul Malek <abdulmalekmonir97@gmail.com>
2024-09-07 22:55:07 +0600

    solved three array problem

---
+++ array-problem.js
@@ -0,0 +1,36 @@
+
+
+// 1. Declare an array
+// Declare an array with 5 elements containing fruits
+// console log the 3rd index element
+// change the value of the 2nd index element to jambura
+// console log the final array
+
+// const fruits = ["apple", "banana", "malta", "pineapple"];
+// console.log(fruits[2]);
+
+// fruits[1] = "orange";
+// console.log(fruits);
+
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+
+// const destination = ['Dhaka', 'Barishal', 'Chattaogram'];
+// destination.push('Khulna');
+// destination.push('Mymensing', 'kushtiya');
+// destination.pop();
+// console.log(destination);
+
+// 3. Checking Array Membership with ‘includes’
+// Instructions:
+
+// Create an array of books containing different book.
+// Use the includes method to check if the array contains a javascript book.
+// Print a message to the console indicating whether the element is present in the array or not.
+
+const books = ['Bangla', 'English', 'Math', 'Science'];
+console.log(books.includes('javascript book'));
+console.log(Array.isArray(books));
